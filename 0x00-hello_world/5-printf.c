42cc0e4fc2e837360a3dae2f56d0dcefa660a6a7
Einstein Ntuli <ntulieinstein@gmail.com>
2022-06-02 14:44:33 -0700

    hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-entry piont for the program
+ *
+ * Return: 0 if there are no errors, not zero if there are errors.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return 0;
+}
