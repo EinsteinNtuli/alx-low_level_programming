0a2cbf34c368979f336b308903c70e8915bc237f
Einstein Ntuli <ntulieinstein@gmail.com>
2022-06-02 07:57:23 -0700

    hello puts 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ *main is the entry point
+ *
+ *the return is always 0 for a successful code
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
