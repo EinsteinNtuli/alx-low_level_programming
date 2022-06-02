78e508b01e11a0e32ca4a5d5f3b672e8b62c46fb
Einstein Ntuli <ntulieinstein@gmail.com>
2022-06-02 08:01:40 -0700

    hello puts 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,9 @@
  *
  *the return is always 0 for a successful code
  */
-int main(void)
+int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }
