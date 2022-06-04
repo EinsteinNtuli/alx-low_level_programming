ddb7dc6f2a54f052227a657b0849c49e60b4fbbd
Einstein Ntuli <ntulieinstein@gmail.com>
2022-06-03 18:56:59 -0700

    random 100

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <time.h>
-#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * main - main block
  * Description: Get random number and print the number
@@ -14,11 +15,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
+	{
 		printf("%i is positive\n", n);
+	}
 	else if (n < 0)
+	{
 		printf("%i is negative\n", n);
+	}
 	else
+	{
 		printf("%i is zero\n", n);
-
+	}
 	return (0);
 }
