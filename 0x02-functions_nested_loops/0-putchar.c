b88a7ea2e4e3ff18d2c896a1b7c5dab7bf17c8ec
Einstein Ntuli <ntulieinstein@gmail.com>
2022-06-07 08:18:28 -0700

    putchar6

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-	char puts[8] = "putchar";
+	char puts[7] = "putchar";
 	int i;
 	int v = '_';
 	putchar(v);
-	for (i = 0; i <= 8; i++)
+	for (i = 0; i < 7; i++)
 	{
 		putchar(puts[i]);
 	}
