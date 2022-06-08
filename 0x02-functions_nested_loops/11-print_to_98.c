7024b25708d117fe0b3e7af106fec83a37204876
Einstein Ntuli <ntulieinstein@gmail.com>
2022-06-07 18:15:15 -0700

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Function that prints natural numbers from n to 98
+ *
+ * @n: The number to be checked
+ */
+void print_to_98(int n)
+{
+
+	if (n < 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n != 98)
+			{
+				printf("%d, ", n);
+			}
+			else
+			{
+				printf("%d\n", n);
+			}
+		}
+	}
+	else if (n == 98)
+	{
+		printf("%d\n", n);
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n != 98)
+			{
+				printf("%d, ", n);
+			}
+			else
+			{
+				printf("%d\n", n);
+			}
+		}
+	}
+}
