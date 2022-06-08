dee0eb8b99bea287d13720afcee20ad7f70aad95
Einstein Ntuli <ntulieinstein@gmail.com>
2022-06-07 17:29:56 -0700

    betty fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- *
+ * print_alphabet_x10 - print all alphabet 10 times
  *
  * Return: 0
  */
 void print_alphabet_x10(void)
 {
 	int i;
+
 	i = 0;
 
-	while (i < 11)
+	while (i < 9)
 	{
 		char r;
 
