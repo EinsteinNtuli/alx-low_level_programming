185f78de1c59c54483c3d37ad8fbeacbd2e09179
Einstein Ntuli <ntulieinstein@gmail.com>
2022-06-07 17:14:21 -0700

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main"
+
+/**
+ *
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int t;
+	i = 0;
+
+	while (i < 11)
+	{
+		char r;
+
+		for (r = 'a'; r <= 'z'; r++)
+		{
+			_putchar(r)
+		}
+		_putchar('\n);
+		i++;
+	}
