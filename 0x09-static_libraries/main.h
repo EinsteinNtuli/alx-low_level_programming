cff8b518eba1d833845bc446e01362ea8f759194
Einstein Ntuli <ntulieinstein@gmail.com>
2022-06-24 16:12:54 -0700

    static_lib

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+/* Define libraries */
+#include <unistd.h>
+#include <ctype.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -22,4 +28,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* MAIN_HEADER */
