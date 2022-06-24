bd531c3669a4490b91ad99b2dcb26e4c0a1d64ba
Einstein Ntuli <ntulieinstein@gmail.com>
2022-06-24 15:29:22 -0700

    to infinity and beyond 2

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,43 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <ctype.h>
 #include "main.h"
-
 /**
- * main - Program that takes in all integer arguments and returns the sum
- * @argc: Number of command line arguments
- * @argv: Array name
- * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
+ *main-sum of positive numbers.
+ *@argc:number of commandline arguments.
+ *@argv:pointer to an array of commmand line arguments.
+ *Return:0-success, non-zero-fail.
  */
 
 int main(int argc, char *argv[])
 {
-	int i, j, length, sum;
-	char *ptr;
+	int sum = 0, i, j;
 
-	if (argc < 2)
-	printf("0\n");
-	else
+	for  (i = 1; i < argc; i++)
 	{
-		sum = 0;
-		for (i = 1; i < argc; i++)
+		if (isNumber(argv[i]) == 0)
 		{
-			ptr = argv[i];
-			length = strlen(ptr);
+			j = atoi(argv[i]);
 
-			for (j = 0; j < length; j++)
-			{
-				if (isdigit(*(ptr + j)) == 0)
-				{
-					printf("Error\n");
-					return (1);
-				}
-			}
-
-			sum += atoi(argv[i]);
+			sum += j;
 		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+/**
+ *isNumber-checks if a string is an integer.
+ *@number:string to be checked.
+ *Return: 0-if is number, 1 if not number.
+ */
+int isNumber(char number[])
+{
+	int i = 0;
 
-		printf("%d\n", sum);
+	/*checking for negative numbers*/
+	if (number[0] == '-')
+		i = 1;
+	for (; number[i] != 0; i++)
+	{
+		/*if (number[i] > '9' || number[i] < '0')*/
+		if (!isdigit(number[i]))
+			return (1);
 	}
 	return (0);
+}
